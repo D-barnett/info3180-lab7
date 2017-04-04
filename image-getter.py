75ed6bd1e00c5e99823996cf457613c71903f479
d_barnett <domoniquebarnett7@gmail.com>
2017-04-04 00:28:36 +0000

    Exercise 1

--- image-getter.py
+++ image-getter.py
@@ -2,23 +2,23 @@ import requests
 from bs4 import BeautifulSoup
 import urlparse
 
+
 url = "https://www.walmart.com/ip/54649026"
 result = requests.get(url)
 soup = BeautifulSoup(result.text, "html.parser")
 
-# This will look for a meta tag with the og:image property
+  # This will look for a meta tag with the og:image property
 og_image = (soup.find('meta', property='og:image') or
                     soup.find('meta', attrs={'name': 'og:image'}))
 if og_image and og_image['content']:
     print og_image['content']
     print ''
 
-# This will look for a link tag with a rel attribute set to 'image_src'
+  # This will look for a link tag with a rel attribute set to 'image_src'
 thumbnail_spec = soup.find('link', rel='image_src')
 if thumbnail_spec and thumbnail_spec['href']:
     print thumbnail_spec['href']
     print ''
 
-
 for link in soup.find_all('a'):
-    print(link.get('href'))
+    print (link.get('href'))

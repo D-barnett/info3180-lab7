89851e7b99968f734dc09418b209c156fb0d6ec0
d_barnett <domoniquebarnett7@gmail.com>
2017-04-07 21:49:24 +0000

    Exercise 1

--- image-getter.py
+++ image-getter.py
@@ -2,26 +2,29 @@ import requests
 from bs4 import BeautifulSoup
 import urlparse
 
-def getimages():
-   url = "https://www.walmart.com/ip/54649026"
-   result = requests.get(url)
-   soup = BeautifulSoup(result.text, "html.parser")
 
-   imagelisting = []
+url = "https://www.walmart.com/ip/54649026"
+result = requests.get(url)
+soup = BeautifulSoup(result.text, "html.parser")
 
-   # This will look for a meta tag with the og:image property
-   og_image = (soup.find('meta', property='og:image') or
+imagelisting = []
+
+# This will look for a meta tag with the og:image property
+og_image = (soup.find('meta', property='og:image') or
 	                    soup.find('meta', attrs={'name': 'og:image'}))
-   if og_image and og_image['content']:
-       imagelisting.append(og_image['content'])
+if og_image and og_image['content']:
+   #print og_image['content']
+   #print ''
+   imagelisting.append(og_image['content'])
 
-   # This will look for a link tag with a rel attribute set to 'image_src'
-   thumbnail_spec = soup.find('link', rel='image_src')
-   if thumbnail_spec and thumbnail_spec['href']:
-       imagelisting.append(thumbnail_spec['href'])
+# This will look for a link tag with a rel attribute set to 'image_src'
+thumbnail_spec = soup.find('link', rel='image_src')
+if thumbnail_spec and thumbnail_spec['href']:
+   #print thumbnail_spec['href']
+   #print ''
+   imagelisting.append(thumbnail_spec['href'])
 
-   for img in soup.findAll("img", src=True):
-       if img["src"] not in imagelisting: # removes duplication of urls
-           imagelisting.append(img['src'])
-           #print imagelisting
-   return imagelisting
+for img in soup.findAll("img", src=True):
+      if img["src"] not in imagelisting: # removes duplication of urls
+         imagelisting.append(img['src'])
+print imagelisting
